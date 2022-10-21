cc1a6dbf5a14de0050c1be5121878ce768e4ddb9
Ben <benawhiting@gmail.com>
2022-10-21 16:18:34 -0700

    Show scores when hovering popular film thumbnails

--- src/components/MovieContainer.js
+++ src/components/MovieContainer.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../stylesheets/MovieContainer.css';
 
 const API_IMG = 'https://image.tmdb.org/t/p/w500/'
 
 const MovieContainer = ({ title, poster, average, release, overview }) => {
+  const [hover, setHover] = useState(false);
+
+  const handleMouseOver = () => {
+    setHover(true);
+  };
+
+  const handleMouseOut = () => {
+    setHover(false);
+  }
+
   return (
-    <div className='poster-container'>
+    <div className='poster-container' onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
       {/* <h1>{title}</h1> */}
       <img src={API_IMG + poster} alt='Movie poster' className='poster'/>
       {/* <div className='average'>{average}</div>
       <div className='release-date'>{release}</div>
       <div className='overview'>{overview}</div> */}
+      {/* Figure out why this isn't making one line on last two posters */}
+      { hover ?
+        <div className='poster-average'>Score: {average}</div>
+        : null
+      }
     </div>
   );
 };
