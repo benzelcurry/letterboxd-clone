e87e86d2626b9de37f54459fa69ba9f0a64d9dc5
benzelcurry <benawhiting@gmail.com>
2022-10-25 11:22:28 -0700

    Create component and dynamic route for movie details pages

---
+++ src/components/MoviePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Nav from './Nav';
+
+const MoviePage = () => {
+  return (
+    <div>
+      <Nav />
+      Movie details will go here.
+    </div>
+  );
+};
+
+export default MoviePage;
--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
+
 import App from './App';
 import Lists from './Lists';
 import Members from './Members';
+import MoviePage from './MoviePage';
 
 const RouteSwitch = () => {
   return (
@@ -11,6 +13,7 @@ const RouteSwitch = () => {
         <Route path='/' element={<App />} />
         <Route path='/lists' element={<Lists />} />
         <Route path='/members' element={<Members />} />
+        <Route path='/films/:movieName' element={<MoviePage />} />
       </Routes>
     </HashRouter>
   );
