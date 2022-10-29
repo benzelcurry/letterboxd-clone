1c4a271ed8f6f00408fbe7d8231e22de2fa1d4d7
Ben <benawhiting@gmail.com>
2022-10-29 13:12:53 -0700

    Make list posters display stack like cards

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 import Nav from './Nav';
-import '../stylesheets/Lists.css'
+import '../stylesheets/Lists.css';
+import Babadook from '../images/list-mocks/babadook.jpg';
+import Lebowski from '../images/list-mocks/lebowski.jpg';
+import Morbius from '../images/list-mocks/morbius.jpg';
+import Spiderman from '../images/list-mocks/spiderman3.jpg';
+import Room from '../images/list-mocks/the-room.jpg';
 
 const Lists = () => {
   return (
@@ -10,6 +15,20 @@ const Lists = () => {
       <div className="lists-header">
         Collect, curate, and share. Lists are the perfect way to group films.
         <div className="lists-start">Start your own list</div>
+      </div>
+      <div className="popular-lists">
+        <div className="popular-list-title">POPULAR THIS WEEK</div>
+        <div className="lists-display">
+          <div className="list">
+            <div className="list-posters">
+              <img src={Babadook} alt='Babadook' className='first'/>
+              <img src={Lebowski} alt='The Big Lebowski' className='list-poster'/>
+              <img src={Morbius} alt='Morbius' className='list-poster'/>
+              <img src={Spiderman} alt='Spiderman 3' className='list-poster'/>
+              <img src={Room} alt='The Room' className='list-poster'/>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
