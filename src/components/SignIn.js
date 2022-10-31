4bd0fb3551364fff80db78825da17a9269e4f10a
Ben <benawhiting@gmail.com>
2022-10-31 15:25:59 -0700

    Complete sign-in popup and add styling

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -7,10 +7,28 @@ const SignIn = ({ setSignIn }) => {
     setSignIn(false);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <div className="sign-in-container">
-      <div onClick={handleClick}>Exit</div>
-      Sign in stuff will go here.
+      <div className='close' onClick={handleClick}>×</div>
+      <form className='sign-in-form' onSubmit={e => handleSubmit(e)}>
+        <div className="sign-in-info">
+          <label htmlFor='username'>Username or Email</label>
+          <input type='text' id='username' name='username'></input>
+        </div>
+        <div className="sign-in-info">
+          <label htmlFor='password'>Password</label>
+          <input type='password' id='password' name='password'></input>
+        </div>
+        <div className="sign-in-remember">
+          <input type='checkbox' id='remember' name='remember'></input>
+          <label htmlFor='remember' className='remember'>Remember me</label>
+        </div>
+        <button className='sign-in'>SIGN IN</button>
+      </form>
     </div>
   );
 };
