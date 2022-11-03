833ee7033f195b7c476409681fe32249ade78c99
benzelcurry <benawhiting@gmail.com>
2022-11-03 12:07:35 -0700

    Update styling of account page

--- src/components/Account.js
+++ src/components/Account.js
@@ -20,8 +20,18 @@ const Account = ({ query, setQuery, searchQuery, setSearchQuery }) => {
   return (
     <div className='account-container'>
       <Nav query={query} setQuery={setQuery} searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-      <div>Welcome, {user?.displayName}</div>
-      <button onClick={handleSignOut}>Sign Out</button>
+      <div className="account-panel">
+        <img src={user.photoURL} alt='Profile picture' className='account-pic' />
+        <div className='account-name'>{user?.displayName}</div>
+        <button className='account-logout' onClick={handleSignOut}>Sign Out</button>
+      </div>
+      <div className="account-info">
+        <div className="account-welcome">Welcome to your profile page!</div>
+        <div className="account-about">
+          <p>Here is where you can access info about your account.</p>
+          <p>There's much to do on this site, though, so get out and access it!</p>
+        </div>
+      </div>
     </div>
   );
 };
